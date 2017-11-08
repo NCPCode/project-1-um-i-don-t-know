e3e6139898e2d7d456e9518450215ceb485fd3e0
Student <CSLAB313-1740@instr.cps.k12.il.us>
2017-11-08 16:15:32 -0600

    Tron bug

--- tron.py
+++ tron.py
@@ -19,21 +19,65 @@ class Player:
 			self.coords[0] -= speed
 
 	def draw(self, window):
-		pygame.draw.rect(window, self.color, (self,coords, self.size))
+		pygame.draw.rect(window, self.color, (self.coords, self.size))
 
 	def check_boundaries(self, screen_size):
-    	for i in [0, 1]:
-      		if self.coords[i] < 0:
-        		self.coords[i] = 0
-      		elif self.coords[i] > screen_size[i] - self.size[i]
-        		self.coords[i] = screen_size[i] - self.size[i]
+		for i in [0, 1]:
+			if self.coords[i] < 0:
+				self.coords[i] = 0
+			elif self.coords[i] > screen_size[i] - self.size[i]:
+				self.coords[i] = screen_size[i] - self.size[i]
 
-    def has_collided(self, player):
-    	return ((self.coords[0] < player.coords[0] + player.size[0]) and
-            (player.coords[0] < self.coords[0] + self.size[0]) and
-            (self.coords[1] < player.coords[1] + player.size[1]) and
-            (player.coords[1] < self.coords[1] + self.size[1]))
+	def has_collided(self, player):
+		return ((self.coords[0] < player.coords[0] + player.size[0]) and
+			(player.coords[0] < self.coords[0] + self.size[0]) and
+			(self.coords[1] < player.coords[1] + player.size[1]) and
+			(player.coords[1] < self.coords[1] + self.size[1]))
 
-    def reset(self):
-    	self.coords = list(self.initial_coords)
+	def reset(self):
+		self.coords = list(self.initial_coords)
 
+WINDOW_SIZE = [1000, 1000]
+PLAYER_SIZE = [10, 10]
+player1 = Player(
+	pygame.Color("red"), 
+	[0, 0],
+	PLAYER_SIZE, 
+	1
+)
+player2 = Player(
+	pygame.Color("blue"),
+	[WINDOW_SIZE[0] - PLAYER_SIZE[0], WINDOW_SIZE[1] - PLAYER_SIZE[1]],
+	PLAYER_SIZE,
+	1
+)
+window = pygame.display.set_mode(WINDOW_SIZE)
+clock = pygame.time.Clock()
+for event in pygame.event.get():
+	if event.type == pygame.QUIT:
+		exit()
+keys = pygame.key.get_pressed()
+if keys[pygame.K_RIGHT]:
+	player1.move('right')
+elif keys[pygame.K_LEFT]:
+	player1.move('left')
+if keys[pygame.K_UP]:
+	player1.move('up')
+elif keys[pygame.K_DOWN]:
+	player1.move('down')
+
+player1.check_boundaries(WINDOW_SIZE)
+player2.check_boundaries(WINDOW_SIZE)
+
+if player1.has_collided(player2):
+	player1.reset()
+	player2.reset()
+
+window.fill(pygame.Color('white'))
+
+player1.draw(window)
+player2.draw(window)
+
+pygame.display.update()
+
+clock.tick(60)
