8ee5eb8991ab61edced8151198289a9da217a27e
Student <CSLAB313-1740@instr.cps.k12.il.us>
2017-11-20 14:59:07 -0600

    Added automatic movement, but still need trail collision detection

--- tron.py
+++ tron.py
@@ -26,9 +26,15 @@ class Player:
 	def check_boundaries(self, screen_size):
 		for i in [0, 1]:
 			if self.coords[i] < 0:
-				self.coords[i] = 0
-			elif self.coords[i] > screen_size[i] - self.size[i]:
 				self.coords[i] = screen_size[i] - self.size[i]
+			elif self.coords[i] > screen_size[i] - self.size[i]:
+				self.coords[i] = 0
+
+	def abs(self, integer):
+		string_int = str(integer)
+		if ('-' in string_int):
+			string_int = string_int[1:]
+		return int(integer)
 
 	def has_collided(self, player, trailplayer1, trailplayer2):
 		collided = False
@@ -45,17 +51,17 @@ class Player:
 		window.fill(pygame.Color('white'))
 		self.coords = list(self.initial_coords)
 
-WINDOW_SIZE = [100, 100]
+WINDOW_SIZE = [200, 200]
 PLAYER_SIZE = [10, 10]
 player1 = Player(
 	pygame.Color("red"), 
-	[0, 0],
+	[(WINDOW_SIZE[0] - PLAYER_SIZE[0]), (WINDOW_SIZE[1] - PLAYER_SIZE[1])/2],
 	PLAYER_SIZE, 
 	1
 )
 player2 = Player(
 	pygame.Color("blue"),
-	[WINDOW_SIZE[0] - PLAYER_SIZE[0], WINDOW_SIZE[1] - PLAYER_SIZE[1]],
+	[0, (WINDOW_SIZE[1] - PLAYER_SIZE[1])/2],
 	PLAYER_SIZE,
 	1
 )
@@ -64,38 +70,55 @@ clock = pygame.time.Clock()
 trailplayer1 = []
 trailplayer2 = []
 window.fill(pygame.Color('white'))
+p_1_direction = 'left'
+p_2_direction = 'right'
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			exit()
-		keys = pygame.key.get_pressed()
-		pygame.draw.rect(window, player1.color, (player1.coords, player1.size))
-		pygame.draw.rect(window, player2.color, (player2.coords, player2.size))
-		if keys[pygame.K_RIGHT]:
-			player1.move('right')
-		elif keys[pygame.K_LEFT]:
-			player1.move('left')
-		if keys[pygame.K_UP]:
-			player1.move('up')
-		elif keys[pygame.K_DOWN]:
-			player1.move('down')
-		trailplayer1.append(player1.coords)
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_d]:
-			player2.move('right')
-		elif keys[pygame.K_a]:
-			player2.move('left')
-		if keys[pygame.K_w]:
-			player2.move('up')
-		elif keys[pygame.K_s]:
-			player2.move('down')
-		trailplayer2.append(player2.coords)
+	keys = pygame.key.get_pressed()
+	pygame.draw.rect(window, player1.color, (player1.coords, player1.size))
+	pygame.draw.rect(window, player2.color, (player2.coords, player2.size))
+	if keys[pygame.K_RIGHT]:
+		player1.move('right')
+		p_1_direction = 'right'
+	elif keys[pygame.K_LEFT]:
+		player1.move('left')
+		p_1_direction = 'left'
+	elif keys[pygame.K_UP]:
+		player1.move('up')
+		p_1_direction = 'up'
+	elif keys[pygame.K_DOWN]:
+		player1.move('down')
+		p_1_direction = 'down'
+	else:
+		player1.move(p_1_direction)
+	trailplayer1.append(player1.coords)
+	if keys[pygame.K_d]:
+		player2.move('right')
+		p_2_direction = 'right'
+	elif keys[pygame.K_a]:
+		player2.move('left')
+		p_2_direction = 'left'
+	if keys[pygame.K_w]:
+		player2.move('up')
+		p_2_direction = 'up'
+	elif keys[pygame.K_s]:
+		player2.move('down')
+		p_2_direction = 'down'
+	else:
+		player2.move(p_2_direction)
+	trailplayer2.append(player2.coords)
+	if (len(trailplayer1) > 1):
+		trailplayer1.remove(trailplayer1[0])
+	if (len(trailplayer2) > 1):
+		trailplayer2.remove(trailplayer2[0])
 
 
 	player1.check_boundaries(WINDOW_SIZE)
 	player2.check_boundaries(WINDOW_SIZE)
 
-	if player1.has_collided(player2, trailplayer1, trailplayer2) or player2.has_collided(player1, trailplayer1, trailplayer2):
+	if player1.has_collided(player2, trailplayer1, trailplayer2):
 		player1.reset()
 		player2.reset()
 		trailplayer1 = []
